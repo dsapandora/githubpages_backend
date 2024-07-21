4e365260872b1563001be872b95ec3b127d2b2c4
Ariel Vernaza <dsapandora@Ariels-MacBook-Pro.local>
2024-07-21 18:30:12 -0500

    Add sengrid integration

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import axios from 'axios';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import sgMail from '@sendgrid/mail';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
@@ -10,6 +12,10 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
 app.get('/pinned-repos/', async (req: Request, res: Response) => {
     const query = `
@@ -45,12 +51,33 @@ app.get('/pinned-repos/', async (req: Request, res: Response) => {
         res.json(response.data);
     } catch (error) {
         if (axios.isAxiosError(error)) {
-            // This type check ensures that the error is an AxiosError and has response properties
             res.status(500).json({ error: error.message });
         } else {
-            // For other types of errors, provide a generic error message
             res.status(500).json({ error: 'An unexpected error occurred' });
         }
+    }
+});
+
+// New endpoint to handle email sending with SendGrid
+app.post('/api/send-email', async (req: Request, res: Response) => {
+    const { name, email, subject, message } = req.body;
+
+    const msg = {
+        to: "${process.env.USER_EMAIL}", 
+        from: 'no-reply@lazyracoon.tech', 
+        subject: subject,
+        text: message,
+        html: `<p><strong>Name:</strong> ${name}</p>
+               <p><strong>Email:</strong> ${email}</p>
+               <p><strong>Message:</strong> ${message}</p>`,
+    };
+
+    try {
+        await sgMail.send(msg);
+        res.status(200).json({ success: true, message: 'Email sent successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Failed to send email' });
     }
 });
 
